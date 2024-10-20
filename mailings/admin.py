e6e253d42d246ca18f2584259c3ded7d311d97fe
alina <khomkolovaalina@gmail.com>
2024-10-20 18:43:23 +0200

    зарегистрировала модели

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailings.models import Client, Message, Mailings
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'full_name', 'comment',)
+    list_filter = ('id',)
+    search_fields = ('email', 'full_name',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'body',)
+    list_filter = ('id',)
+    search_fields = ('title',)
+
+
+@admin.register(Mailings)
+class MailingsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'periodicity', 'date_first_dispatch',)
+    list_filter = ('id',)
+    search_fields = ('client', 'message')
